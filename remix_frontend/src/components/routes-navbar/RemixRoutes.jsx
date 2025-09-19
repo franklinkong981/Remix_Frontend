5253c95567946927b434dbbdfdba5ce17a698f39
Franklin Kong <franklinkong981@gmail.com>
2025-09-19 10:44:32 -0700

    Added Routes component

---
+++ remix_frontend/src/components/routes-navbar/RemixRoutes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Routes, Route, Navigate} from "react-router-dom";
+
+import ProtectedRoute from "./ProtectedRoute.jsx";
+
+import Homepage from "../home/Homepage.jsx";
+import LoginForm from "../auth/LoginForm.jsx";
+import SignupForm from "../auth/SignupForm.jsx";
+
+/**
+ * The component for site-wide routes rendered by the App component.
+ * 
+ * Parts of the site should only be visitable when logged in. Those routes
+ * are wrapped by <ProtectedRoute>, which is an authorization component.
+ * 
+ * Visitng a non-existent route redirects to the homepage.
+ */
+function RemixRoutes({signUpFunc, loginFunc}) {
+  return (
+    <div className="Routes pt-5">
+      <Routes>
+        <Route exact path="/login" element={<LoginForm loginFunc={loginFunc} />} />
+        <Route path="/signup" element={<SignupForm signUpFunc={signUpFunc} />} />
+
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default RemixRoutes;
