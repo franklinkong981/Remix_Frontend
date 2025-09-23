e23baa84891d5aabf9195429f34dfc34a4fcc93d
Franklin Kong <franklinkong981@gmail.com>
2025-09-23 12:59:32 -0700

    Added UpdateProfileForm component for user to update profile

---
+++ remix_frontend/src/components/profile/UpdateProfileForm.jsx
@@ -0,0 +1,107 @@
+import React, {useState, useContext} from "react";
+import {useNavigate} from "react-router-dom";
+
+import RemixApi from "../../api/api.js";
+
+import Alert from "../reusables/Alert.jsx";
+
+import CurrentUserContext from "../../contexts/currentUserContext.jsx";
+
+/**
+ * Profile editing form that logged in user can fill out to update their profile information.
+ * Top-level component of /profile route.
+ * 
+ * Handles changes to local form state on the frontend. Submitting form calls RemixApi to save
+ * updated user information on the backend and also the frontend through currentUserInfo in CurrentUserContext,
+ * which triggers reloading throughout the site to updated user information.
+ * 
+ * Alert messages will be displayed if user inputs don't match expected criteria, confirmation of a successful save is a simple Alert component.
+ */
+function UpdateProfileForm() {
+  const {currentUserInfo, setCurrentUserInfo} = useContext(CurrentUserContext);
+
+  const [profileFormData, setProfileFormData] = useState({
+    username: currentUserInfo.username,
+    email: currentUserInfo.firstName
+  });
+  const [profileFormErrors, setProfileFormErrors] = useState([]);
+
+  const [updateProfileSuccessful, setUpdateProfileSuccessful] = useState(false);
+
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setProfileFormData(profileFormData => ({
+      ...profileFormData,
+      [name]: value
+    }));
+  }
+
+  /**
+   * On form submission,
+   * - First attempt to update user information on backend and report any errors.
+   * - Upon successful update, clears any error messages on form, shows save-confirmed message.
+   * - Finally, updates current user info on frontend through CurrentUserContext and reloads current user info throughout the site.
+   */
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    let updateProfileFormValues = {
+      username: profileFormData.username,
+      email: profileFormData.email
+    };
+
+    let username = currentUserInfo.username;
+    let updatedUserInfo;
+
+    try {
+      updatedUserInfo = await RemixApi.updateUserProfile(username, updateProfileFormValues);
+    } catch(errors) {
+      setProfileFormErrors(errors);
+      return;
+    }
+
+    setProfileFormData(profileFormData => ({...profileFormData}));
+    setProfileFormErrors([]);
+    setUpdateProfileSuccessful(true);
+
+    // triggers current user info throughout the site.
+    setCurrentUserInfo(updatedUserInfo);
+  }
+
+  return (
+    <div className="UpdateProfileForm col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+      <h2 className="UpdateProfileForm-title">Edit Profile Information.</h2>
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-username-field">Username:</label>
+              <p id="UpdateProfileForm-username-field" className="form-control-plaintext">{profileFormData.username}</p>
+            </div>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-email-field">Email:</label>
+              <input id="UpdateProfileForm-email-field"
+                className="form-control"
+                type="email"
+                name="email"
+                value={profileFormData.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            {profileFormErrors.length ? <Alert alertTexts={profileFormErrors} /> : null}
+
+            {updateProfileSuccessful ? <Alert alertType="success" alertTexts={["Profile updated successfully!"]} /> : null}
+
+            <button 
+              className="UpdateProfileForm-button btn btn-primary btn-block mt-4" 
+              onClick={handleSubmit}
+            >Save Updated Info</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateProfileForm;
