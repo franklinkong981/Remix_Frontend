547010a0a3eb39588aed03f10b092c2086058d3f
Franklin Kong <franklinkong981@gmail.com>
2025-09-29 12:02:37 -0700

    Added RecipeReviewList component for listing out recipe reviews

---
+++ remix_frontend/src/components/recipes/RecipeReviewList.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from "react";
+
+import RecipeReview from "./RecipeReview.jsx";
+
+import {v4 as uuidv4} from "uuid";
+
+/**
+ * Component that shows a list of recipe reviews, with each recipe review being a RecipeReview component.
+ * Is used for pages that show a specific list of recipe reviews (ex. all recipe reviews belonging to a specific user).
+ * 
+ * No state in this component, the list of recipe reviews (an array of recipe review objects) to display is passed in.
+ * 
+ * Contains RecipeReview components.
+ */
+function RecipeReviewList({recipeReviews}) {
+  return (
+    <div className="RecipeReviewList col-md-8 offset-md-2">
+      {recipesReviews.length ? (
+        <div className="RecipeReviewList-list">
+          {recipesReviews.map(recipeReview => (
+            <RecipeReview key={uuidv4()} 
+              recipeId={recipeReview.recipeId} 
+              recipeName={recipeReview.recipeName}
+              title={recipeReview.title}
+              content={recipeReview.content} 
+              createdAt={recipeReview.createdAt}
+            />
+          ))}
+        </div>
+      ) : <p className="RecipeReviewList-no-recipe-reviews">No recipe reviews found.</p>}
+    </div>
+  );
+}
+
+export default RecipeReviewList;
