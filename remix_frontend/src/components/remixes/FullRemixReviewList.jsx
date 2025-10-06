250aaca699ae67013ad4842fc559c78f34a6d2da
Franklin Kong <franklinkong981@gmail.com>
2025-10-06 12:10:11 -0700

    Finished FullRemixReviewsList component and thus finished RecipeDetail and RemixDetail page components

--- remix_frontend/src/components/remixes/FullRemixReviewList.jsx
+++ remix_frontend/src/components/remixes/FullRemixReviewList.jsx
@@ -19,35 +19,35 @@ function FullRemixReviewList() {
   const params = useParams();
   const location = useLocation();
   //locationRemix = state passed in from the /remixes/:remixId aka remix details page route, contains {remixId, remixName}
-  const locationRecipe = location.state;
+  const locationRemix = location.state;
   const [listOfRemixReviews, setListOfRemixReviews] = useState(null);
 
   // Retrieve remix's reviews data from database. While this is happening,
   // the "loading" text appears on the page.
   // After the remix review list is retrieved, displays them each as a RemixReview component in a RemixReviewList component.
-  useEffect(function loadRecipeReviewsWhenMounted() {
-    async function fetchRecipeReviews() {
-      const allRecipeReviews = await RemixApi.getAllRecipeReviews(params.recipeId);
-      setListOfRecipeReviews(allRecipeReviews);
+  useEffect(function loadRemixReviewsWhenMounted() {
+    async function fetchRemixReviews() {
+      const allRemixReviews = await RemixApi.getAllRemixReviews(params.remixId);
+      setListOfRemixReviews(allRemixReviews);
     }
-    fetchRecipeReviews();
+    fetchRemixReviews();
   }, []);
 
-  //if listOfRecipeReviews is empty, will still be truthy, so won't show loading screen.
-  if (!listOfRecipeReviews) {
-    return <h1 className="FullRecipeReviewList-loading">Loading...</h1>
+  //if listOfRemixReviews is empty, will still be truthy, so won't show loading screen.
+  if (!listOfRemixReviews) {
+    return <h1 className="FullRemixReviewList-loading">Loading...</h1>
   }
   
   return (
-    <div className="FullRecipeReviewList">
-      <h1 className="FullRecipeReviewList-header">
-        All Recipe Reviews for <Link className="FullRecipeReviewList-details-page-link" to={`/recipes/${locationRecipe.recipeId}`}>
-          {locationRecipe.recipeName}
+    <div className="FullRemixReviewList">
+      <h1 className="FullRemixReviewList-header">
+        All Remix Reviews for <Link className="FullRemixReviewList-details-page-link" to={`/remixes/${locationRemix.remixId}`}>
+          {locationRemix.remixName}
         </Link>
       </h1>
-      {listOfRecipeReviews.length ? (
-        <RecipeReviewList recipeReviews={listOfRecipeReviews}/>
-      ) : <p className="FullRecipeReviewList-no-reviews">This recipe currently doesn't have any recipe reviews.</p>}
+      {listOfRemixReviews.length ? (
+        <RemixReviewList remixReviews={listOfRemixReviews}/>
+      ) : <p className="FullRemixReviewList-no-reviews">This remix currently doesn't have any remix reviews.</p>}
     </div>
   );
 }
