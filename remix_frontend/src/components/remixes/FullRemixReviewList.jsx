ce0f7171fd0224c0f46a5c8f75c6b6435dface6e
Franklin Kong <franklinkong981@gmail.com>
2025-10-06 11:55:50 -0700

    Finished the remix detail component

---
+++ remix_frontend/src/components/remixes/FullRemixReviewList.jsx
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from "react";
+
+import {useParams, useLocation, Link} from "react-router-dom";
+
+import RecipeReviewList from "./RecipeReviewList.jsx";
+import RemixApi from "../../api/api.js";
+
+//import {v4 as uuidv4} from "uuid";
+
+/**
+ * Top-level component for the page that shows the full list of recipe reviews belonging to a specific recipe.
+ * When page loads, loads the list of recipe reviews belonging to the recipe and displays them.
+ * 
+ * Route: /recipes/:recipeId/reviews
+ * 
+ * Contains RecipeReviewList component.
+ */
+function FullRecipeReviewList() {
+  const params = useParams();
+  const location = useLocation();
+  //locationRecipe = state passed in from the /recipes/:recipeId aka recipe details page route, contains {recipeId, recipeName}
+  const locationRecipe = location.state;
+  const [listOfRecipeReviews, setListOfRecipeReviews] = useState(null);
+
+  // Retrieve recipe's reviews data from database. While this is happening,
+  // the "loading" text appears on the page.
+  // After the recipe review list is retrieved, displays them each as a RecipeReview component in a RecipeReviewList component.
+  useEffect(function loadRecipeReviewsWhenMounted() {
+    async function fetchRecipeReviews() {
+      const allRecipeReviews = await RemixApi.getAllRecipeReviews(params.recipeId);
+      setListOfRecipeReviews(allRecipeReviews);
+    }
+    fetchRecipeReviews();
+  }, []);
+
+  //if listOfRecipeReviews is empty, will still be truthy, so won't show loading screen.
+  if (!listOfRecipeReviews) {
+    return <h1 className="FullRecipeReviewList-loading">Loading...</h1>
+  }
+  
+  return (
+    <div className="FullRecipeReviewList">
+      <h1 className="FullRecipeReviewList-header">
+        All Recipe Reviews for <Link className="FullRecipeReviewList-details-page-link" to={`/recipes/${locationRecipe.recipeId}`}>
+          {locationRecipe.recipeName}
+        </Link>
+      </h1>
+      {listOfRecipeReviews.length ? (
+        <RecipeReviewList recipeReviews={listOfRecipeReviews}/>
+      ) : <p className="FullRecipeReviewList-no-reviews">This recipe currently doesn't have any recipe reviews.</p>}
+    </div>
+  );
+}
+
+export default FullRecipeReviewList;
---
+++ remix_frontend/src/components/remixes/RemixDetail.jsx
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+
+import RemixApi from "../../api/api.js";
+
+import RecipeRemixesPreview from "./RecipeRemixesPreview.jsx";
+import RecipeReviewsPreview from "./RecipeReviewsPreview.jsx";
+
+/**
+ * Remix Details page top-level component.
+ * This is the page that shows full detailed information about a remix including ingredients, directions, number of servings, cooking time,
+ * the user who created it, etc. which will then be followed by the most recently added review of the remix as a RemixReviewsPreview component, which contains
+ * a link to the page that lists out all remix reviews of the remix.
+ * 
+ * Route is /remixes/:remixId
+ * 
+ * Contains the RemixReviewsPreview component.
+ * 
+ */
+function RemixDetail() {
+  const {remixId} = useParams();
+  const [remixDetailedInfo, setRemixDetailedInfo] = useState(null);
+
+  //retrieve detailed info about remix, including its  most recently added review from the Remix API.
+  // While this is happening, the word "Loading" is displayed on the screen. Remix information won't be displayed 
+  // until the detailed information of the remix is returned from the API.
+  useEffect(function loadRemixDetailsWhenMounted() {
+    async function fetchRemixDetails() {
+      const remix = await RemixApi.getRemixDetails(remixId);
+      setRemixDetailedInfo(remix);
+    }
+    fetchRemixDetails();
+  }, [remixId]);
+
+  if (!remixDetailedInfo) return <h1>Loading...</h1>
+
+  return (
+    <div className="RemixDetail col-md-8 offset-md-2">
+      <img className="RemixDetail-image" src={remixDetailedInfo.imageUrl} alt={`Picture of ${remixDetailedInfo.name}`}></img>
+      <h1 className="RemixDetail-name">{remixDetailedInfo.name}</h1>
+      <h2 className="RemixDetail-description">{remixDetailedInfo.description}</h2>
+      <p className="RemixDetail-original-recipe">Remix of 
+        <Link className="RemixDetail-original-recipe-link font-weight-bold" to={`/recipes/${remixDetailedInfo.originalRecipeId}`}>
+          {remixDetailedInfo.originalRecipe}
+        </Link>
+      </p>
+      <p className="RemixDetail-author">Created by {remixDetailedInfo.remixAuthor} on {remixDetailedInfo.createdAt}.</p>
+      <hr />
+      <h3 className="RemixDetails-details-header">Recipe Details:</h3>
+      <p className="RemixDetails-purpose">Purpose of this remix: {remixDetailedInfo.purpose}</p>
+      <p className="RemixDetails-ingredients">Ingredients: {remixDetailedInfo.ingredients}</p>
+      <p className="RemixDetails-directions">Instructions: {remixDetailedInfo.directions}</p>
+      <p className="RemixDetails-cooking-time">Cooking Time: {remixDetailedInfo.cookingTime > 0 ? remixDetailedInfo.cookingTime : "N/A"}</p>
+      <p className="RemixDetails-servings">Servings: {remixDetailedInfo.servings > 0 ? remixDetailedInfo.servings : "N/A"}</p>
+      <hr />
+      <RemixReviewsPreview remixId={remixDetailedInfo.id} remixName={remixDetailedInfo.name} remixReview={remixDetailedInfo.mostRecentRemixReview}/>
+    </div>
+  );
+}
+
+
+export default RemixDetail;
