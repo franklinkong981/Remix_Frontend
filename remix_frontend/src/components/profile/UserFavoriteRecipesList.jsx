a2289dfc3bbd279caa74057352b2c45992df6b9e
Franklin Kong <franklinkong981@gmail.com>
2025-10-22 10:14:43 -0700

    Added UserFavoriteRecipesList and UserFavoriteRemixesList components

--- remix_frontend/src/components/profile/UserFavoriteRecipesList.jsx
+++ remix_frontend/src/components/profile/UserFavoriteRecipesList.jsx
@@ -8,41 +8,41 @@ import RemixApi from "../../api/api.js";
 //import {v4 as uuidv4} from "uuid";
 
 /**
- * Top-level component for the page that shows the list of recipes belonging to a specific user.
- * When page loads, loads the list of recipes belonging to the user and displays them.
+ * Top-level component for the page that shows the list of a user's favorite recipes.
+ * When page loads, loads the list of favorite recipes as a RecipeList component and displays them.
  * 
  * Route: /users/:username/favorites/recipes
  * 
  * Contains RecipeList component.
  */
-function UserRecipeList() {
+function UserFavoriteRecipeList() {
   const params = useParams();
-  const [listOfRecipes, setListOfRecipes] = useState(null);
+  const [favoriteRecipesList, setFavoriteRecipesList] = useState(null);
 
-  // Retrieve user's recipes data from database. While this is happening,
+  // Retrieve user's favorite recipes data from database. While this is happening,
   // the "loading" text appears on the page.
-  // After the recipe list is retrieved, displays them each as a RecipeCard component in a RecipeList component
+  // After the favorite recipe list is retrieved, displays them each as a RecipeCard component in a RecipeList component.
   useEffect(function loadRecipesWhenMounted() {
-    async function fetchRecipes() {
-      const userRecipes = await RemixApi.getUsersRecipes(params.username);
-      setListOfRecipes(userRecipes);
+    async function fetchFavoriteRecipes() {
+      const favoriteRecipes = await RemixApi.getFavoriteRecipes(params.username);
+      setFavoriteRecipesList(favoriteRecipes);
     }
-    fetchRecipes();
+    fetchFavoriteRecipes();
   }, []);
 
-  //if listOfRecipes is empty, will still be truthy, so won't show loading screen.
-  if (!listOfRecipes) {
-    return <h1 className="UserRecipeList-loading">Loading...</h1>
+  //if favoriteRecipesList is empty, will still be truthy, so won't show loading screen.
+  if (!favoriteRecipesList) {
+    return <h1 className="UserFavoriteRecipeList-loading">Loading...</h1>
   }
   
   return (
-    <div className="UserRecipeList">
-      <h1 className="UserRecipeList-header">Your Recipes</h1>
-      {listOfRecipes.length ? (
-        <RecipeList recipes={listOfRecipes}/>
-      ) : <p className="UserRecipeList-no-companies">You currently have not created any recipes.</p>}
+    <div className="UserFavoriteRecipeList">
+      <h1 className="UserFavoriteRecipeList-header">Your Favorite Recipes</h1>
+      {favoriteRecipesList.length ? (
+        <RecipeList recipes={favoriteRecipesList}/>
+      ) : <p className="UserFavoriteRecipeList-no-recipes">You currently do not have any favorite recipes.</p>}
     </div>
   );
 }
 
-export default UserRecipeList;
+export default UserFavoriteRecipeList;
---
+++ remix_frontend/src/components/profile/UserFavoriteRemixesList.jsx
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from "react";
+
+import {useParams} from "react-router-dom";
+
+import RemixList from "../remixes/RemixList.jsx";
+import RemixApi from "../../api/api.js";
+
+//import {v4 as uuidv4} from "uuid";
+
+/**
+ * Top-level component for the page that shows the list of a user's favorite remixes.
+ * When page loads, loads the list of favorite remixes as a RemixList component and displays them.
+ * 
+ * Route: /users/:username/favorites/remixes
+ * 
+ * Contains RemixList component.
+ */
+function UserFavoriteRemixList() {
+  const params = useParams();
+  const [favoriteRemixesList, setFavoriteRemixesList] = useState(null);
+
+  // Retrieve user's favorite remixes data from database. While this is happening,
+  // the "loading" text appears on the page.
+  // After the favorite remix list is retrieved, displays them each as a RemixCard component in a RemixList component.
+  useEffect(function loadRemixesWhenMounted() {
+    async function fetchFavoriteRemixes() {
+      const favoriteRemixes = await RemixApi.getFavoriteRemixes(params.username);
+      setFavoriteRemixesList(favoriteRemixes);
+    }
+    fetchFavoriteRemixes();
+  }, []);
+
+  //if favoriteRemixesList is empty, will still be truthy, so won't show loading screen.
+  if (!favoriteRemixesList) {
+    return <h1 className="UserFavoriteRemixList-loading">Loading...</h1>
+  }
+  
+  return (
+    <div className="UserFavoriteRemixList">
+      <h1 className="UserFavoriteRemixList-header">Your Favorite Remixes</h1>
+      {favoriteRemixesList.length ? (
+        <RemixList remixes={favoriteRemixesList}/>
+      ) : <p className="UserFavoriteRemixList-no-remixes">You currently do not have any favorite remixes.</p>}
+    </div>
+  );
+}
+
+export default UserFavoriteRemixList;
