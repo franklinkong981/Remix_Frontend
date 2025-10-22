471314c5260b3edd0a2ca6375c8b7ff8151120c9
Franklin Kong <franklinkong981@gmail.com>
2025-10-22 11:58:18 -0700

    Updated RecipeCard component to include add/remove from Favorites button

--- remix_frontend/src/components/recipes/RecipeCard.jsx
+++ remix_frontend/src/components/recipes/RecipeCard.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { Link } from "react-router-dom";
 
 import CurrentUserContext from "../../contexts/currentUserContext.jsx";
@@ -12,9 +12,32 @@ import "./RecipeCard.css";
  * For RecipeCards of recipes belonging to the logged in user, there will also be a button to edit the recipe.
  * 
  * Rendered by RecipeList component to show a card for each recipe.
+ * 
+ * In addition, there will be a button at the bottom to either add the recipe to the user's list of favorite recipes
+ * or remove it from the user's list of favorite recipes.
  */
 function RecipeCard({id, name, description, recipeAuthor="", imageUrl, createdAt}) {
-  const {currentUserInfo} = useContext(CurrentUserContext);
+  const {currentUserInfo, isRecipeInFavorites, addRecipeToFavorites, removeRecipeFromFavorites} = useContext(CurrentUserContext);
+
+  const [isFavorite, setIsFavorite] = useState();
+
+  //When user adds/removes the recipe from their favorites list, isRecipeInFavorites function return value 
+  // is updated in the context, which udpates the text on the Favorites button.
+  useEffect(function updateIsFavoriteStatus() {
+    setIsFavorite(isRecipeInFavorites(id))
+  }, [id, isRecipeInFavorites]);
+
+  async function handleFavoritesClick(evt) {
+    if (isFavorite) {
+      if (!(isRecipeInFavorites(id))) return;
+      await removeRecipeFromFavorites(id);
+      setIsFavorite(false);
+    } else {
+      if (isRecipeInFavorites(id)) return;
+      await addRecipeToFavorites(id);
+      setIsFavorite(true);
+    }
+  }
 
   return (
     <section className="RecipeCard card">
@@ -35,6 +58,11 @@ function RecipeCard({id, name, description, recipeAuthor="", imageUrl, createdAt
               Update Recipe
             </Link>
         }
+        <button 
+          className="RecipeCard-favorite-button btn btn-danger font-weight-bold float-right"
+          onClick={handleFavoritesClick}>
+            {isFavorite ? "Remove From Favorites" : "Add to Favorites"}
+        </button>
       </div>
     </section>
   );
